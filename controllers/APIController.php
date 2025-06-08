9009011d2c49a2e75d7d6f2507e37397575be390
AdrianFelixJoxus <162724771+AdrianFelixJoxus@users.noreply.github.com>
2025-06-08 12:38:26 -0700

    Update APIController.php
    Se regreso linea de codigo citasServicios

--- controllers/APIController.php
+++ controllers/APIController.php
@@ -13,6 +13,30 @@ class APIController {
         echo json_encode($servicios);
     }
 
+     public static function guardar() {
+        // Almacena la cita y devuelve el ID
+          $cita = new Cita($_POST);
+          $resultado = $cita->guardar();
+          $id = $resultado["id"];
+
+         // Almacena la cita y el servicio
+         // Almacena los servicios con el ID de la cita
+         $idServicios = explode(",",$_POST["servicios"]);
+
+         foreach($idServicios as $idServicio) {
+            $args = [
+                "citaId" => $id,
+                "servicioId" => $idServicio
+            ];
+            $citaServicio = new CitaServicio($args);
+            $citaServicio->guardar();
+         }
+
+         // Retornamos una respuesta
+         
+        echo json_encode(["resultado" => $resultado]);
+    }
+
     
 
     public static function eliminar() {
